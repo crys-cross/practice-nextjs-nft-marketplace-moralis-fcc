cbb8386fd0d6ff9f3636a3148b063cff5922c336
CRYS <mariano.christopher123@gmail.com>
2022-08-13 22:48:06 +0800

    added constants for ABI and address from backend script

--- components/NFTBox.tsx
+++ components/NFTBox.tsx
@@ -1,9 +1,31 @@
+import type { NextPage } from "next"
 import { useState } from "react"
+import { useWeb3Contract } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import nftAbi from "../constants/BasicNft.json"
 
-const NFTBox = ({ price, nftAddress, tokenId, marketplaceAddress, seller }) => {
+interface NFTBoxProps {
+    price?: number
+    nftAddress: string
+    tokenId: string
+    marketplaceAddress: string
+    seller?: string
+}
+
+const NFTBox: NextPage<NFTBoxProps> = ({
+    price,
+    nftAddress,
+    tokenId,
+    marketplaceAddress,
+    seller,
+}: NFTBoxProps) => {
     //call the token URi and then call the image uri to show the image(2 api request)
     //then save image as a state variable in this component
     const [imageURI, setimageIRI] = useState("")
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+    })
     const updateUI = async () => {
         // get the tokenURI
         // using the image tag from the tokenURI, get the image
